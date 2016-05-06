c733e731fd2d416f5497d8e8ca03c2160e031849
bonedeng <bonedeng@bonedengs-MacBook-Pro.local>
2016-05-06 18:30:35 +0800

    [dengpeng]: home view controller extension added

--- sagittarius-swift/home/controller/HomeViewController.swift
+++ sagittarius-swift/home/controller/HomeViewController.swift
@@ -23,16 +23,14 @@ class HomeViewController: UIViewController{
         print("enmu is \(Constant.contestStatus.Cancelled.rawValue)")
         
         self.tableData = Dictionary<String, AnyObject>();
-        
         dataSource = HomeTableViewDataSource(aIdentifier: self.identifier)
+        
         delegate = HomeTableViewDelegate(aCellSelect: { (indexPath) -> Void in
             print("clicked");
         })
         
         self.navigationController?.navigationBarHidden = true
-        
-        self.setNavigationView()
-        self.setLayoutTable()
+        self.setupUi();
         self.requestData()
     }
     
@@ -53,23 +51,6 @@ class HomeViewController: UIViewController{
             }) { (response) -> Void in
                 print("error is \(response)")
         }
-    }
-    
-    func setNavigationView() -> Void {
-        let homeNavigationView = HomeNavigationView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 64.0))
-        self.view.addSubview(homeNavigationView)
-    }
-    
-    func setLayoutTable() -> Void {
-        
-        self.table = UITableView(frame: CGRect(x: 0, y: 64, width: CGRectGetWidth(UIScreen.mainScreen().bounds), height: CGRectGetHeight(UIScreen.mainScreen().bounds)-64-49), style: .Plain)
-        self.table.backgroundColor = UIColor(hexString: "#030c16")
-        self.table.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.identifier)
-        self.table.separatorStyle = .None
-        self.table.dataSource = self.dataSource
-        self.table.delegate = self.delegate
-        self.view.addSubview(self.table)
-        
     }
     
     override func didReceiveMemoryWarning() {
---
+++ sagittarius-swift/home/extension/HomeViewController+setupUI.swift
@@ -0,0 +1,43 @@
+//
+//  HomeViewController+setupUI.swift
+//  sagittarius-swift
+//
+//  Created by bonedeng on 5/6/16.
+//  Copyright © 2016 xingdongyou. All rights reserved.
+//
+
+import UIKit
+import ObjectiveC
+private var homeAssociationKey: String = "extensionProperty"
+extension HomeViewController {
+    
+    var extensionProperty : String {
+        get {
+            return objc_getAssociatedObject(self, &homeAssociationKey) as! String
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &homeAssociationKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    func setupUi()->Void {
+        self.setNavigationView()
+        self.setLayoutTable()
+    }
+    
+    func setNavigationView() -> Void {
+        let homeNavigationView = HomeNavigationView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 64.0))
+        self.view.addSubview(homeNavigationView)
+    }
+    
+    func setLayoutTable() -> Void {
+        
+        self.table = UITableView(frame: CGRect(x: 0, y: 64, width: CGRectGetWidth(UIScreen.mainScreen().bounds), height: CGRectGetHeight(UIScreen.mainScreen().bounds)-64-49), style: .Plain)
+        self.table.backgroundColor = UIColor(hexString: "#030c16")
+        self.table.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.identifier)
+        self.table.separatorStyle = .None
+        self.table.dataSource = self.dataSource
+        self.table.delegate = self.delegate
+        self.view.addSubview(self.table)
+        
+    }
+}
